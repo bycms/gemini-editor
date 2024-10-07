1cbdc5f6f227abc010d99e655bffadf100ae4573
BoyuanChen-1113 <cby2022.1113@gmail.com>
2024-10-07 20:47:17 +0800

    /

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "/@google/generative-ai";
-import MarkdownIt from 'markdown-it';
+import MarkdownIt from '/markdown-it';
 
 let API_KEY = "AIzaSyBmVYOrJrwN0l4cODZOW7NwXl8ysg-kl8E";
 let form = document.querySelector('form');
